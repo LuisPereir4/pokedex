02b3469588e2e10dc073441e31094c15c469c468
Luis_Rebelo <luis_rebelo@dellteam.com>
2021-10-23 21:56:50 -0300

    :tada: Backend base project

---
+++ pokedex-backend/src/test/java/com/pereir4/pokedexbackend/PokedexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereir4.pokedexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
